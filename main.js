654efea8cabce24ec67bb8a14f1663db16566912
Sonicfangs <Sonicfangs>
2016-09-24 08:51:57 -0400

    added time stamps

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
 
-
+  var date = new Date();
   var addNotification = function(notifType,deviceName){
     switch (notifType) {
       case 0://Neutral
         $('body').append('<div class=\'notif-bar\'>' + deviceName
-          + ' has been opened <\/div>');
+          + ' has been opened ' + printDate() + ' <\/div>');
         break;
       case 1://green
         $('body').append('<div class=\'notif-bar approved\'>' + deviceName
-          + ' has been opened <\/div>');
+          + ' has been opened ' + printDate() + '<\/div>');
         break;
       case 2://red
         $('body').append('<div class=\'notif-bar not-approved\'>' + deviceName
-          + ' has been opened <\/div>');
+          + ' has been opened ' + printDate() + '<\/div>');
         break;
       case 3://error
         break;
@@ -22,5 +22,57 @@ $(document).ready(function(){
     }
   };
 
+  var epochToMonth = function(monthNumber){
+    monthNumber++;
+    switch(monthNumber){
+      case 1: return "Jan";
+      break;
+
+      case 2: return "Feb";
+      break;
+
+      case 3: return "Mar";
+      break;
+
+      case 4: return "Apr";
+      break;
+
+      case 5: return "May";
+      break;
+
+      case 6: return "Jun";
+      break;
+
+      case 7: return "Jul";
+      break;
+
+      case 8: return "Aug";
+      break;
+
+      case 9: return "Sep";
+      break;
+
+      case 10: return "Oct";
+      break;
+
+      case 11: return "Nov";
+      break;
+
+      case 12: return "Dec";
+      break;
+
+      default:
+        return "Pick a month";
+    }
+  };
+
+  var printDate = function(){
+
+    var s = "" + epochToMonth(date.getMonth()) + " " +date.getDay() + ", "
+            + date.getFullYear() + "  " + date.getHours() + ":" + date.getMinutes();
+    return s;
+  };
 addNotification(0,"Device");
+addNotification(1,"Device");
+addNotification(2,"Device");
 });
