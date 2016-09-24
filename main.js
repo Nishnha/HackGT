2ad731d5d7cab91e5eb2c7d30714d5f64249c8ac
Sonicfangs <Sonicfangs>
2016-09-24 14:51:47 -0400

    JS Edits

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 $(document).ready(function(){
 
-  var date = new Date();
-  var addNotification = function(notifType,deviceName){
+  var addNotification = function(notifType,deviceName,timeStamp){
+    var date = new Date(timeStamp*1000);
     switch (notifType) {
       case 0://Neutral
-        $('body').append('<div class=\'notif-bar\'>' + deviceName
-          + ' has been opened ' + printDate() + ' <\/div>');
+        $('body').append('<div class=\'notif-bar\'><div class=\"notif-message\"'
+         + ">" + deviceName + ' has been opened <\/div><div class=\"notif-timestamp\">'
+          + printDate(date) + '<\/div> <\/div>');
         break;
       case 1://green
-        $('body').append('<div class=\'notif-bar approved\'>' + deviceName
-          + ' has been opened ' + printDate() + '<\/div>');
+        $('body').append('<div class=\'notif-bar approved\'><div class=\"notif-message\"'
+         + ">" + deviceName + ' has been opened <\/div><div class=\"notif-timestamp\">'
+          + printDate(date) + '<\/div> <\/div>');
         break;
       case 2://red
-        $('body').append('<div class=\'notif-bar not-approved\'>' + deviceName
-          + ' has been opened ' + printDate() + '<\/div>');
+        $('body').append('<div class=\'notif-bar not-approved\'><div class=\"notif-message\"'
+         + ">" + deviceName + ' has been opened without permission<\/div><div class=\"notif-timestamp\">'
+          + printDate(date) + '<\/div> <\/div>');
         break;
       case 3://error
         break;
@@ -66,17 +69,20 @@ $(document).ready(function(){
     }
   };
 
-  var printDate = function(){
-
-    if(date.getMinutes() < 10)
-      var min = "0" + date.getMinutes();
-    if(date.getHours() > 12)
-      var hours = date.getHours()-12
-    var s = "" + epochToMonth(date.getMonth()) + " " +date.getDay() + ", "
-            + date.getFullYear() + "  " + date.getHours() + ":" + min;
+  var printDate = function(epoch){
+    var min = epoch.getMinutes();
+    var hours = epoch.getHours();
+    if(epoch.getMinutes() < 10)
+      var min = "0" + epoch.getMinutes();
+    if(epoch.getHours() > 12)
+      hours = hours - 12;
+    if(hours < 10)
+      hours = " " + hours;
+    var s = "" + epochToMonth(epoch.getMonth()) + " " +epoch.getDay() + ", "
+            + epoch.getFullYear() + "  " + hours + ":" + min;
     return s;
   };
-addNotification(0,"Device");
-addNotification(1,"Device");
-addNotification(2,"Device");
+addNotification(0,"Device",1474531672);
+addNotification(1,"Device",1474727457);
+addNotification(2,"Device",1474727457);
 });
